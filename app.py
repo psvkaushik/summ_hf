bbfb6501ca2d46d7dd49a17fe467d78013ef3c61
psvkaushik <86014345+psvkaushik@users.noreply.github.com>
2024-02-21 01:58:51 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+from transformers import pipeline
+import gradio as gr
+
+model = pipeline('summarization')
+def predict(prompt):
+    output = model(prompt)[0]['summary_text']
+    return output
+
+with gr as demo:
+    textbox = gr.Textbox(placeholder="Enter the text to summarize", lines=4)
+    gr.Interface(fn=predict, inputs=textbox, outputs='text')
+
+demo.launch()
